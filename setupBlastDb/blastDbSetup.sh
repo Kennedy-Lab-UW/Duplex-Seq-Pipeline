a0caeae926f59ba594c358947d8c4f3c01de8a7a
Brendan Kohrn <kohrnb@uw.edu>
2021-03-30 09:52:44 -0700

    Improvements to blastDbSetup script Add path support Add file type checking

--- setupBlastDb/blastDbSetup.sh
+++ setupBlastDb/blastDbSetup.sh
@@ -1,15 +1,25 @@
+set -e
+set -u
 set -x
 
 inGenome="$1"
 inTaxID="$2"
 myPath=${0/blastDbSetup.sh/}
 
+genomeName=$(basename ${inGenome})
+baseGenomeName=${genomeName%%.fa?(sta)}
+
+if [ genomeName == baseGenomeName ]; then
+    echo "Genome must be a .fa or .fasta file"
+    exit 1
+fi
+
 makeblastdb \
 -dbtype nucl \
--title ${inGenome/.fa*/} \
--out ${inGenome/.fa*/}_db \
+-title ${baseGenomeName} \
+-out ${baseGenomeName}_db \
 -in ${inGenome} \
 -taxid ${inTaxID}
 
-blastdb_aliastool -dblist "${inGenome/.fa*/}_db" \
--dbtype nucl -out ${inGenome/.fa*/}_db -title "${inGenome/.fa*/}"
+blastdb_aliastool -dblist "${baseGenomeName}_db" \
+-dbtype nucl -out ${baseGenomeName}_db -title "${baseGenomeName}"
